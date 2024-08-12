09839550eeec1da42f9a37ad86e043e8596a3d39
Felipe Alferez <alferezfelipe@gmail.com>
2024-08-12 12:52:57 -0500

    feat: add validations and improve findOne

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { Logger } from "@nestjs/common";
+import { Logger, ValidationPipe } from "@nestjs/common";
 import { envs } from "./config/envs";
 
 async function bootstrap() {
   const logger = new Logger("GATEWAY MAIN");
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
   app.setGlobalPrefix("api");
   await app.listen(envs.port);
   logger.log(`GATEWAY RUNNING ON PORT ${envs.port}`);
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Inject, Post, Query } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
+import { firstValueFrom } from "rxjs";
 import { PaginationDto } from "src/common/dto/pagination.dto";
 import { PRODUCT_SERVICE } from "src/config/services";
 
@@ -10,12 +11,21 @@ export class ProductsController {
   ) {}
 
   @Post()
-  createProduct() {
-    return "create product";
+  createProduct(@Query() product: any) {
+    return this.productsClient.send("create_product", { product });
   }
 
   @Get()
   findAll(@Query() pagination: PaginationDto) {
+    console.log(pagination);
     return this.productsClient.send("get_all_products", pagination);
+  }
+
+  @Get()
+  async findById(@Query() id: string) {
+    const product = await firstValueFrom(
+      this.productsClient.send("get_product_id", { id })
+    );
+    return product;
   }
 }
