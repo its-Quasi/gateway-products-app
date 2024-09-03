e08b45253a8ef9f0a653338ff97290dbd3767001
quasi <alferezfelipe@gmail.com>
2024-09-02 21:46:30 -0500

    feat: update dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,20 +1,11 @@
-import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
-import { OrderStatus } from "src/enum/order-status";
+import { Type } from "class-transformer";
+import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
+import { OrderItemDto } from "./order-item.dto";
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number
-
-  @IsNumber()
-  @IsPositive()
-  totalItems: number
-
-  @IsEnum(OrderStatus, { message: `Posible status are ${OrderStatus}` })
-  @IsOptional()
-  status: OrderStatus = OrderStatus.PENDING
-
-  @IsBoolean()
-  @IsOptional()
-  paid: boolean = false
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[]
 }
